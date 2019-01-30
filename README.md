8e42e98a0b2926b1aa3ac5756a4238b3baee04ea
deoladed <leo.robert.mk@gmail.com>
2019-01-30 20:01:31 +0100

    Job. Done

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Bienvenu dans le blog des familles !
 Ruby version : 2.5.1
 Rails vers : 5.2.1
-
+--------------------------------------
 ## Contenu
 Une DB contenant des users qui commentent,likent et creent des articles qui ont elles meme des categories. Au top, skyblog n'a qu'a se tenir!
 
-### Courage jeune correcteur !
+### Utilisation
+Apres le *git clone*, faite biensur un *bundle install* puis un *rails db:seed* pour creer la DB =)
+
+
+#### Courage jeune correcteur !
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# require 'faker'
-# 10.times do
-#   user = User.create!(last_name: Faker::Name.last_name, first_name: Faker::Name.first_name, email: Faker::Internet.email)
-# end
-# p "10 utilisateurs ont été créés"
-
-# require 'faker'
-# 10.times do |i|
-#   user = Article.create!(title: Faker::BreakingBad.character, content: Faker::ChuckNorris.fact, user: User.find(i+1))
-# end
-# p "10 articles ont été créés"
-
-# # require 'faker'
-# # 5.times do |i|
-# #   user = Category.create!(name: Faker::Hobbit.character)
-# # end
-# # p "10 articles ont été créés"
-
-# 5.times do |i|
-# article = Article.find(i+16)
-# article.update(category: Category.find(i+1))
-# end
-
-
 require 'faker'
-14.times do |i|
-	Like.create(article: Article.find(rand(2..15)), user: User.find(rand(1..10)))
-	p 'like created'
+
+# On cree les users
+10.times do
+  User.create!(last_name: Faker::Name.last_name, first_name: Faker::Name.first_name, email: Faker::Internet.email)
 end
+p "10 utilisateurs ont été créés"
+
+# Puis les articles
+10.times do
+  Article.create!(title: Faker::BreakingBad.character, content: Faker::ChuckNorris.fact, user: User.all.sample)
+end
+p "10 articles ont été créés"
+
+# les categories d'articles
+5.times do
+  Category.create!(name: Faker::Hobbit.character)
+end
+p "10 articles ont été créés"
+
+#On associe chaque article a une categorie :
+10.times do |i|
+Article.find(i+1).update(category: Category.all.sample)
+end
+
+# On cree des commentaires, chacun associes a un article et un user
+15.times do
+	Comment.create(content: Faker::ChuckNorris.fact, article_id: rand(1..10), user_id: rand(1..10))
+end
+p "15 commentaires crees"
+
+# On cree enfin des likes, associes a un article et un user, un user like un article quoi.
+15.times do |i|
+	Like.create(article: Article.find(rand(1..10)), user: User.find(rand(1..10)))
+end
+	p '15 like created'
+
+# Pour randomiser, 3 techniques : .all.sample  class: Class.find(rand(1..10))  class_id: rand(1..10)
+
